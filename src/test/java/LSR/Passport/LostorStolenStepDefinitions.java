c96798c62ffb07978f9f3515596ead6fadb5a0af
sreekanth <sreekanth.bongunuri@gmail.com>
2015-02-06 10:50:10 +0000

    adding extra steps for child under 16 reporting lost passport

--- src/test/java/LSR/Passport/LostorStolenStepDefinitions.java
+++ src/test/java/LSR/Passport/LostorStolenStepDefinitions.java
@@ -15,12 +15,14 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
+@SuppressWarnings("deprecation")
 public class LostorStolenStepDefinitions {
 
 	private WebDriver driver;
 	
 	@Given("^an instance of \"(.*?)\" browser$")
 	public void anInstanceOfBrowser(final String browserName) throws Throwable {
+		this.driver.manage().window().maximize();
 		if(browserName.equalsIgnoreCase("Firefox")){
 			  this.driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"),DesiredCapabilities.firefox());
 		}
@@ -39,11 +41,38 @@ public class LostorStolenStepDefinitions {
 	   this.driver.get(url);
 	}
 
-	@SuppressWarnings("deprecation")
 	@Then("^the lost or stolen user interface is presented$")
 	public void theLostOrStolenUserInterfaceIsPresented() throws Throwable {
 	   Assert.assertTrue(this.driver.findElement(By.id("startForm")).isDisplayed());
 
 
 	}
+	
+	@Then("^the user clicks start now$")
+	public void theUserClicksStartNow() throws Throwable {
+	 this.driver.findElement(By.id("startForm")).click();
+	}
+
+	@Then("^selects child under (\\d+) and clicks continue$")
+	public void selectsChildUnderAndClicksContinue(int arg1) throws Throwable {
+	 this.driver.findElement(By.id("ReportType_holder_holder_1")).click();
+	 this.driver.findElement(By.id("submitNext")).click();
+	}
+
+	@Then("^enters valid name and telephone number$")
+	public void entersValidNameAndTelephoneNumber() throws Throwable {
+	    this.driver.findElement(By.id("ReportType_reporter_child_fullName")).sendKeys("sree");
+	    this.driver.findElement(By.id("ReportType_reporter_child_telephone")).sendKeys("07950731996");
+	}
+
+	@Then("^clicks submit$")
+	public void clicksSubmit() throws Throwable {
+		 this.driver.findElement(By.id("submitNext")).click();
+	}
+
+	@Then("^close the browser$")
+	public void closeTheBrowser() throws Throwable {
+		 this.driver.quit();
+	}
+
 }
--- src/test/java/resources/LostPassport.feature
+++ src/test/java/resources/LostPassport.feature
@@ -1,10 +1,14 @@
 Feature: Reporting lost or stolen passport
     As an Automation Test Team
     In order to test the lost or stolen user interface 
-    I want to a web driver project driven by BDD story
+    I want a web driver project driven by BDD story
 
     Scenario: Connect to LSR ui 
         Given an instance of "firefox" browser 
         When the url "https://www-preprod-lsr.pex.ips.gov.uk/" is opened
         Then the lost or stolen user interface is presented
-       
+        And the user clicks start now
+        Then selects child under 16 and clicks continue
+        And enters valid name and telephone number
+        And clicks submit
+        And close the browser
